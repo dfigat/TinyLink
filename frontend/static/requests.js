09684dd9d4628f0d0d10717b57b59cace5e0a1c4
Arystokrates <casimir3thegreat@gmail.com>
2025-05-08 08:43:45 +0200

    rebase

--- frontend/static/requests.js
+++ frontend/static/requests.js
@@ -2,17 +2,20 @@ async function createTinyLink() {
     const longLink = document.querySelector('.tiny_link_input').value
     const outputContainer = document.querySelector('.tiny_link_output')
     try {
-        const response = await fetch('http://link.cbpio.pl/api/v1.0/short', {
+        const response = await fetch('http://link.cbpio.pl:8080/api/v1.0/short/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ longLink: longLink })
+            body: JSON.stringify({ long_link: longLink })
         })
 
         const data = await response.json()
 
-        if (response.ok)
+        if(response.status == 400)
+            outputContainer.textContent = 'There is an issue with the provided link'
+
+        else if (response.ok)
             outputContainer.textContent = data.code
 
     } catch (error) {
@@ -20,7 +23,6 @@ async function createTinyLink() {
         outputContainer.textContent = 'An error has occured while creating tiny link: ' + error
     }
 }
-console.log(document.querySelector('.tiny_link_input').value);
 
 const submitButton = document.querySelector('.tiny_link_submit')
 submitButton.addEventListener('click', createTinyLink)
